14b86ebcb379e74fc142861a4445199f73864125
Jacob Patton <83788852+rustybucket-cloud@users.noreply.github.com>
2025-07-16 17:06:01 -0600

    fix error

--- init.lua
+++ init.lua
@@ -256,17 +256,17 @@ require('lazy').setup({
 
   {
     'akinsho/bufferline.nvim',
-    -- keys = {
-    --   { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle pin" },
-    --   { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers" },
-    --   { "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete other buffers" },
-    --   { "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete buffers to the right" },
-    --   { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete buffers to the left" },
-    --   { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
-    --   { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
-    --   { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
-    --   { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
-    -- },
+    keys = {
+      { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle pin" },
+      { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers" },
+      { "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete other buffers" },
+      { "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete buffers to the right" },
+      { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete buffers to the left" },
+      { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
+      { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+      { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
+      { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+    },
     version = "*",
     dependencies = 'nvim-tree/nvim-web-devicons',
     config = function()
@@ -277,6 +277,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>1', '<Cmd>BufferLineGoToBuffer 1<CR>', { desc = "Go to first buffer" })
     end,
   },
+
 
   -- formatter
   -- {
