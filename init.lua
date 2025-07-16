2d9a459d58870bf1669b3b7208aff222610909ed
Jacob Patton <83788852+rustybucket-cloud@users.noreply.github.com>
2025-07-16 16:58:18 -0600

    update config

--- init.lua
+++ init.lua
@@ -325,7 +325,6 @@ require('lazy').setup({
   --     },
   --   },
   -- }
-  { "nvim-neotest/nvim-nio" },
 
   {
     "nvim-neotest/neotest",
@@ -378,9 +377,39 @@ require('lazy').setup({
     end,
   },
 
+  -- {
+  --   'github/copilot.vim',
+  --   config = function()
+  --     vim.g.copilot_no_tab_map = true
+  --     vim.api.nvim_set_keymap("i", "<C-l>", 'copilot#Accept("<CR>")', { expr = true, silent = true, noremap = true })
+  --   end
+  -- },
   {
-    'github/copilot.vim'
+    "supermaven-inc/supermaven-nvim",
+    config = function()
+      require("supermaven-nvim").setup({
+        keymaps = {
+          accept_suggestion = '<C-l>',
+        }
+      })
+    end,
   },
+
+  -- {
+  --   "CopilotC-Nvim/CopilotChat.nvim",
+  --   dependencies = {
+  --     { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+  --     { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+  --   },
+  --   -- config = function()
+  --   --   vim.keymap.set('n', '<leader>cc', '<CMDCopilotChatOpen<CR>', { desc = 'Open Copilot Chat' })
+  --   -- end,
+  --   build = "make tiktoken", -- Only on MacOS or Linux
+  --   opts = {
+  --     -- See Configuration section for options
+  --   },
+  --   -- See Commands section for default commands if you want to lazy load on them
+  -- },
 
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
