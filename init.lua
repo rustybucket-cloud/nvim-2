ab40e5193fdd621854c5eca2b37415366c0fb441
Jacob Patton <83788852+rustybucket-cloud@users.noreply.github.com>
2025-08-22 16:26:28 -0600

    update

--- init.lua
+++ init.lua
@@ -278,6 +278,19 @@ require('lazy').setup({
     end,
   },
 
+  {
+    "sindrets/diffview.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- optional, for icons
+    },
+    config = function()
+      require("diffview").setup()
+      vim.keymap.set('n', '<leader>do', '<cmd>DiffviewOpen<cr>', { desc = "Open diffview"})
+      vim.keymap.set('n', '<leader>dc', '<cmd>DiffviewClose<cr>', { desc = "Close diffview"})
+    end,
+  },
+
 
   -- formatter
   -- {
@@ -395,6 +408,27 @@ require('lazy').setup({
       })
     end,
   },
+
+  {
+    'NickvanDyke/opencode.nvim',
+    dependencies = { 'folke/snacks.nvim', },
+    ---@type opencode.Config
+    opts = {
+      -- Your configuration, if any
+    },
+    -- stylua: ignore
+    keys = {
+      { '<leader>ot', function() require('opencode').toggle() end, desc = 'Toggle embedded opencode', },
+      { '<C-t>', function() require('opencode').toggle() end, desc = 'Toggle embedded opencode', mode = "t", },
+      { '<leader>oA', function() require('opencode').ask() end, desc = 'Ask opencode', mode = 'n', },
+      { '<leader>oa', function() require('opencode').ask('@selection: ') end, desc = 'Ask opencode about selection', mode = 'v', },
+      { '<leader>op', function() require('opencode').select_prompt() end, desc = 'Select prompt', mode = { 'n', 'v', }, },
+      { '<leader>on', function() require('opencode').command('session_new') end, desc = 'New session', },
+      { '<leader>oy', function() require('opencode').command('messages_copy') end, desc = 'Copy last message', },
+      { '<S-C-u>',    function() require('opencode').command('messages_half_page_up') end, desc = 'Scroll messages up', },
+      { '<S-C-d>',    function() require('opencode').command('messages_half_page_down') end, desc = 'Scroll messages down', },
+    },
+  }
 
   -- {
   --   "CopilotC-Nvim/CopilotChat.nvim",
